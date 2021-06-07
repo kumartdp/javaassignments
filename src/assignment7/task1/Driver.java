8893bfb53f399b39c3389168d6eae65a6f020b45
saikumar <saikumar.madagoni@zemosolabs.com>
2021-06-07 14:40:26 +0530

    java assignments

---
+++ src/assignment7/task1/Driver.java
@@ -0,0 +1,17 @@
+package assignment7.task1;
+
+
+
+public class Driver {
+    public static void main(String[]args) {
+        Rodent g = new Gerbil();
+        g.behaviour();
+        Rodent m=new Mouse();
+        m.behaviour();
+        Rodent h=new Hamster();
+        h.behaviour();
+    }
+
+
+
+}
